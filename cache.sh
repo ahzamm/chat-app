54c3f15400d3b7b3c32600295d49dd84c0a2da78
ahzam <ahzamahmed6@gmail.com>
2024-11-15 17:15:20 +0500

    update cache file

--- cache.sh
+++ cache.sh
@@ -1,4 +1,9 @@
-php artisan config:clear
-php artisan route:clear
-php artisan cache:clear
-php artisan view:clear
+/opt/lampp/bin/php artisan config:clear
+/opt/lampp/bin/php artisan route:clear
+/opt/lampp/bin/php artisan cache:clear
+/opt/lampp/bin/php artisan view:clear
+
+# /opt/lampp/bin/php artisan serve
+# /opt/lampp/bin/php artisan reverb:start
+# /opt/lampp/bin/php artisan queue:work
+# npm run dev
